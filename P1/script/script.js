d388780ae7bee6fabae59a45fa1b252bafb2a5b3
FaaizHaikal <faaizhaikal@gmail.com>
2023-09-04 10:30:47 +0700

    feat: add header, homepage, and about

---
+++ P1/script/script.js
@@ -0,0 +1,34 @@
+function opentab(tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tab-contents");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  
+  tablinks = document.getElementsByClassName("tab-links");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active","");
+  }
+  
+  document.getElementById(tabName).style.display = "block";
+  event.currentTarget.className += " active";
+}
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove("active");
+        document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+      });
+    }
+  })
+}
