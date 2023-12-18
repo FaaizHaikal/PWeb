74562c95fbaceab58f760beafb6fed0c7e4899b2
FaaizHaikal <faaizhaikal@gmail.com>
2023-12-18 16:47:32 +0700

    feat: initialize final project

---
+++ FP/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Math Quiz</title>
+    <link rel="stylesheet" href="./style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/5f60200bd3.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <div class="form-container">
+      <form action="#">
+        <h1>Login</h1>
+        <div class="input-box">
+          <input type="email" placeholder="Username">
+          <i class="fa-solid fa-envelope"></i>
+        </div>
+        <div class="input-box">
+          <input type="password" placeholder="Password">
+          <i class="fa-solid fa-lock"></i>
+        </div>
+        <div class="forgot-box">
+          <a href="#">Forgot password?</a>
+        </div>
+        <button type="submit" class="btn">Login</button>
+        <div class="register-link">
+          Not a member? <a href="#">Register now</a>
+        </div>
+      </form>
+    </div>
+  </body>
+<html>
